62018d875792204a991eeba43c80f052ec4966c4
WeslAyan007 <olibriceweslay@gmail.com>
2026-07-11 16:32:54 -0400

    first commit

--- Projet/script.js
+++ Projet/script.js
@@ -1,7 +1,7 @@
-fetch(
-  'https://api.restcountries.com/v3.1/all',
-  { headers: { 'Authorization': 'Bearer rc_live_50c43f06b93445bc9ea8c914cbdbc47a' } }
+
+  fetch(
+  'https://api.restcountries.com/countries/v5?q=canada',
+  { headers: { 'Authorization': 'Bearer rc_live_61192c8f8c6d4bb69f716668bf5558ef' } }
 )
   .then(function (response) { return response.json(); })
-  .then(function (data) { console.log(data); })
-  .catch(function (error) { console.error('Error fetching countries:', error); });
+  .then(function (data) { console.log(data); });
